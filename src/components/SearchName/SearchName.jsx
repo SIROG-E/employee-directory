29e0c520535808fae60a91ea3f2cd5d1ec9edaf7
Evelyn Goris <goris.evelyn@gmail.com>
2020-10-31 22:52:59 -0500

    added import DataAreaContext

--- src/components/SearchName/SearchName.jsx
+++ src/components/SearchName/SearchName.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import "../SearchName/SearchName.css"
+import React, { useContext } from 'react';
+import '../SearchName/SearchName.css';
+import DataAreaContext from '../../utils/DataAreaContext';
 
 const SearchName = () => {
+    const context = useContext(DataAreaContext);
     return (
         <div className="searchbox">
             <div className="input-group">
-                {/* <div className="input-group-prepend">
-                    <span className="input-group-text" id="">
-                        Search
-            </span>
-                </div> */}
+                
                 <input
                     className="form-control mr-sm-2"
                     type="search"
                     placeholder="Search"
                     aria-label="Search"
+                    onChange={e => context.handleSearchChange(e)}
                 />
             </div>
         </div>
