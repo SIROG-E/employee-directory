f8e3daeb2e8d11d0ceb60f3670bc253a00698e7a
Evelyn Goris <goris.evelyn@gmail.com>
2020-10-31 22:49:04 -0500

    fixed paths of imports

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import DataArea from '../DataArea/DataArea'
 
 const Main = () => {
     return (
         <div>
-            
+           <DataArea /> 
         </div>
     );
 };
